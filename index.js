492fe2938382162577135d30b32154f7ebcedf68
chenruipeng <1259903760@qq.com>
2024-03-08 10:55:05 +0800

    feat: 获取视频元信息

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
 const ffmpeg = require('fluent-ffmpeg')
 const fs = require('fs')
-const path = require('path')
-const minioClient = require('./minio')
-
-// ffmpeg('./test.mp4')
-//   .thumbnails({
-//     folder: './screenshot',
-//     count: 4,
-// });
-
-// ffmpeg('./screenshot/tn_1.png').input('./screenshot/tn_2.png').input('./screenshot/tn_3.png').input('./screenshot/tn_4.png').complexFilter([
-//   {
-//     filter: 'vstack',
-//     options: '4',
-//   }
-// ]).output('output.jpg').run()
-
-// ffmpeg -i a.jpg -i b.jpg -filter_complex hstack output.jpg
-// ffmpeg -i tn_1.png -i tn_2.png -i tn_3.png -i tn_4.png -filter_complex "[0][1][2][3]hstack=inputs=4" output.jpg
+// const minioClient = require('./minio')
 
 // ffmpeg -y -i "test.mp4" -frames 1 -vf "select=not(mod(n\,100)),scale=320:180,tile=10X10" "thumbnail.jpg"
 // ffmpeg -y -i "test.mp4" -frames 1 -vf "thumbnail=n=100,scale=320:180,tile=10X10" thumbnail.jpg
 
+// const ffmpegSync = async (filename) => {
+//   return new Promise((resolve, reject) => {
+//     ffmpeg(`temp/${filename}.mp4`)
+//     .frames(1)
+//     .videoFilters("select='not(mod(n\,100))'", 'scale=320:180', 'tile=10X10')
+//     .output(`temp/${filename}.jpg`)
+//     .on("end", () => {
+//       resolve()
+//     })
+//     .on('error', (err) => {
+//       return reject(new Error(err))
+//     })
+//     .run()
+//   })
+// }
 
-// ffmpeg(fs.createReadStream(path.resolve("./test.mp4"))).frames(1).videoFilters("select='not(mod(n\,100))'", 'scale=320:180', 'tile=10X10').output("thumbnail7.jpg").run();
+// const getPreview = async () => {
+//   const filename = '1709710284055'
+//   await minioClient.fGetObject('test', `${filename}.mp4`, `temp/${filename}.mp4`)
+//   await ffmpegSync(filename)
+//   await fs.unlinkSync(`temp/${filename}.mp4`)
+// }
 
-const ffmpegSync = async (filename) => {
-  return new Promise((resolve, reject) => {
-    ffmpeg(`temp/${filename}.mp4`)
-    .frames(1)
-    .videoFilters("select='not(mod(n\,100))'", 'scale=320:180', 'tile=10X10')
-    .output(`temp/${filename}.jpg`)
-    .on("end", () => {
-      resolve()
-    })
-    .on('error', (err) => {
-      return reject(new Error(err))
-    })
-    .run()
-  })
-  
-}
+// getPreview()
 
-
-const getPreview = async () => {
-  const filename = '1709710284055'
-  await minioClient.fGetObject('test', `${filename}.mp4`, `temp/${filename}.mp4`)
-  await ffmpegSync(filename)
-  await fs.unlinkSync(`temp/${filename}.mp4`)
-}
-
-
-getPreview()
+ffmpeg.ffprobe('deadpool.mp4', (err, metadata) => {
+  const { avg_frame_rate } = metadata.streams[0]
+  console.log(avg_frame_rate);
+})
