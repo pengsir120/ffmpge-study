4372152bfb2e092b15a1ac3efd9cb5ae9ba4e98a
chenruipeng <1259903760@qq.com>
2024-03-09 20:07:49 +0800

    feat: 视频输出多张缩略图

--- index.js
+++ index.js
@@ -30,7 +30,63 @@ const fs = require('fs')
 
 // getPreview()
 
-ffmpeg.ffprobe('deadpool.mp4', (err, metadata) => {
-  const { avg_frame_rate } = metadata.streams[0]
-  console.log(avg_frame_rate);
-})
+// ffmpeg.ffprobe('test.mp4', (err, metadata) => {
+//   const { avg_frame_rate } = metadata.streams[0]
+//   console.log(avg_frame_rate);
+//   console.log(metadata);
+// })
+
+const getVideoMetaData = async (filename) => {
+  return new Promise((resolve, reject) => {
+    ffmpeg.ffprobe(filename, (err, metadata) => {
+      if(err) {
+        reject(new Error(err))
+      }else {
+        resolve(metadata)
+      }
+    })
+  })
+}
+
+const getVideoThumbPics = async (filename, timeStep) => {
+  const metadata = await getVideoMetaData(filename)
+  const { r_frame_rate, nb_frames } = metadata.streams[0]
+  const fps = Number(r_frame_rate.slice(0, 2))
+  let picNum = nb_frames / (fps * timeStep * 100)
+  if(picNum != Math.floor(picNum)) {
+    picNum += 1
+  }
+  for(let i = 0; i < picNum; i++) {
+    ffmpeg(filename)
+    .frames(1)
+    .videoFilters(`select='gte(n\, ${i * fps * timeStep * 100})'`, `select='not(mod(n\,${fps * timeStep}))'`, 'scale=320:180', 'tile=10X10')
+    .output(`${filename.split('.')[0]}-${i}.jpg`)
+    .run()
+  }
+}
+
+getVideoThumbPics('spider.mp4', 4)
+
+// ffmpeg('test.mp4')
+// .frames(1)
+// .videoFilters("select='gte(n\, 0)'", "select='not(mod(n\,240))'", 'scale=320:180', 'tile=10X10')
+// .output(`test11.jpg`)
+// .run()
+
+// ffmpeg('test.mp4')
+// .frames(1)
+// .videoFilters("select='gte(n\, 24000)'", "select='not(mod(n\,240))'", 'scale=320:180', 'tile=10X10')
+// .output(`test22.jpg`)
+// .run()
+
+// ffmpeg('test.mp4')
+// .frames(1)
+// .videoFilters("select='gte(n\, 36000)'", "select='not(mod(n\,240))'", 'scale=320:180', 'tile=10X10')
+// .output(`test33.jpg`)
+// .run()
+
+// ffmpeg('test.mp4')
+// .frames(1)
+// .videoFilters("select='gte(n\, 72000)'", "select='not(mod(n\,240))'", 'scale=320:180', 'tile=10X10')
+// .output(`test44.jpg`)
+// .run()
