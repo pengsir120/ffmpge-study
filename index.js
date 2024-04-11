0e23baeb884628f7b3150f12986d3d3a21061e97
chenruipeng <1259903760@qq.com>
2024-04-11 17:50:30 +0800

    feat: 获取文件夹下一级文件目录所有视频时长

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { log } = require('console')
 const ffmpeg = require('fluent-ffmpeg')
 const fs = require('fs')
+const path = require('path')
+const { getVideoTime } = require('./utils/getVideoInfo')
 // const minioClient = require('./minio')
 
 // ffmpeg -y -i "test.mp4" -frames 1 -vf "select=not(mod(n\,100)),scale=320:180,tile=10X10" "thumbnail.jpg"
@@ -49,24 +51,24 @@ const getVideoMetaData = async (filename) => {
   })
 }
 
-const getVideoThumbPics = async (filename, timeStep) => {
-  const metadata = await getVideoMetaData(filename)
-  const { r_frame_rate, nb_frames } = metadata.streams[0]
-  const fps = Number(r_frame_rate.slice(0, 2))
-  let picNum = nb_frames / (fps * timeStep * 100)
-  if(picNum != Math.floor(picNum)) {
-    picNum += 1
-  }
-  for(let i = 0; i < picNum; i++) {
-    ffmpeg(filename)
-    .frames(1)
-    .videoFilters(`select='gte(n\, ${i * fps * timeStep * 100})'`, `select='not(mod(n\,${fps * timeStep}))'`, 'scale=320:180', 'tile=10X10')
-    .output(`${filename.split('.')[0]}-${i}.jpg`)
-    .run()
-  }
-}
+// const getVideoThumbPics = async (filename, timeStep) => {
+//   const metadata = await getVideoMetaData(filename)
+//   const { r_frame_rate, nb_frames } = metadata.streams[0]
+//   const fps = Number(r_frame_rate.slice(0, 2))
+//   let picNum = nb_frames / (fps * timeStep * 100)
+//   if(picNum != Math.floor(picNum)) {
+//     picNum += 1
+//   }
+//   for(let i = 0; i < picNum; i++) {
+//     ffmpeg(filename)
+//     .frames(1)
+//     .videoFilters(`select='gte(n\, ${i * fps * timeStep * 100})'`, `select='not(mod(n\,${fps * timeStep}))'`, 'scale=320:180', 'tile=10X10')
+//     .output(`${filename.split('.')[0]}-${i}.jpg`)
+//     .run()
+//   }
+// }
 
-getVideoThumbPics('spider.mp4', 4)
+// getVideoThumbPics('spider.mp4', 4)
 
 // ffmpeg('test.mp4')
 // .frames(1)
@@ -103,18 +105,40 @@ getVideoThumbPics('spider.mp4', 4)
 //   }
 // })
 
-const dirs = ['operatingInstructions']
-const transformDirPic = async(dirs) => {
-  for(let i = 0; i < dirs.length; i++) {
-    const files = fs.readdirSync(dirs[i])
-    // 如果文件夹不存在就创建
-    if(!fs.existsSync(`${dirs[i]}/${dirs[i]}`)) {
-      fs.mkdirSync(`${dirs[i]}/${dirs[i]}`)
-    }
-    for(let j = 0; j < files.length; j++) {
-      console.log(files[j]);
-      ffmpeg(`${dirs[i]}/${files[j]}`).output(`${dirs[i]}/${dirs[i]}/${j}.jpg`).run()
-    }
+// const dirs = ['operatingInstructions']
+// const transformDirPic = async(dirs) => {
+//   for(let i = 0; i < dirs.length; i++) {
+//     const files = fs.readdirSync(dirs[i])
+//     // 如果文件夹不存在就创建
+//     if(!fs.existsSync(`${dirs[i]}/${dirs[i]}`)) {
+//       fs.mkdirSync(`${dirs[i]}/${dirs[i]}`)
+//     }
+//     for(let j = 0; j < files.length; j++) {
+//       console.log(files[j]);
+//       ffmpeg(`${dirs[i]}/${files[j]}`).output(`${dirs[i]}/${dirs[i]}/${j}.jpg`).run()
+//     }
+//   }
+// }
+// transformDirPic(dirs)
+
+
+const fileRead = async() => {
+  const dirPath = 'F:/DownKyi-1.6.1/Media'
+  const files = fs.readdirSync(dirPath)
+  const videoPromiseList = []
+  let duration = 0
+  for(let i = 0; i < files.length; i++) {
+    const firstDir = fs.readdirSync(path.resolve(dirPath, files[i]))
+    firstDir.filter(item => item.lastIndexOf('mp4') > 0).forEach(filePath => {
+      videoPromiseList.push(getVideoMetaData(path.resolve(dirPath, files[i], filePath)))
+    })
   }
+  Promise.all(videoPromiseList).then(resList => {
+    resList.forEach(({streams}) => {
+      duration += streams[0].duration
+    })
+    console.log(getVideoTime(duration));
+  })
 }
-transformDirPic(dirs)
+
+fileRead()
